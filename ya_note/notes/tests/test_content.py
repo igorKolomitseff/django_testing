92fc17ba0d723658489d7fa2c012ee79eaf8ba7e
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2024-04-13 19:41:07 +0300

    Refactor test_content.py in the notes app

--- ya_note/notes/tests/test_content.py
+++ ya_note/notes/tests/test_content.py
@@ -1,73 +1,44 @@
-from django.contrib.auth import get_user_model
-from django.test import Client, TestCase
-from django.urls import reverse
+from .constants import UrlsConstants
+from .set_up_test_data import TestData
 
 from notes.forms import NoteForm
 from notes.models import Note
 
 
-User = get_user_model()
-
-
-class TestContent(TestCase):
+class TestContent(TestData):
 
     @classmethod
-    def setUpTestData(cls):
-        cls.author = User.objects.create(username='Author')
-        cls.authenticated_user = User.objects.create(
-            username='AuthenticatedUser'
-        )
-        cls.author_client = Client()
-        cls.author_client.force_login(cls.author)
-        cls.authenticated_client = Client()
-        cls.authenticated_client.force_login(cls.authenticated_user)
-        cls.note = Note.objects.create(
-            title='Заголовок',
-            text='Текст',
-            slug='test',
-            author=cls.author
-        )
+    def setUpTestData(cls, create_single_note=False):
+        super().setUpTestData(create_single_note=True)
 
-    def test_notes_list_for_different_users(self):
-        for client, note_in_list_status, user, note_in_list in (
-            (self.author_client, True,
-             'автора заметки', 'заметка есть'),
-            (self.authenticated_client, False,
-             'стороннего авторизированного пользователя',
-             'чужой заметки нет')
-        ):
-            with self.subTest(client=client):
-                response = client.get(reverse('notes:list'))
-                object_list = response.context['object_list']
-                self.assertIs(
-                    self.note in object_list,
-                    note_in_list_status,
-                    (
-                        f'Убедитесь, что {note_in_list} в контексте страницы '
-                        f'со списком заметок для {user}.'
-                    )
-                )
+    def test_notes_list(self):
+        self.assertIn(
+            self.note,
+            self.author_client.get(
+                UrlsConstants.NOTES_LIST_URL
+            ).context['object_list']
+        )
+        note = Note.objects.get(id=self.note.id)
+        self.assertEqual(self.note.title, note.title)
+        self.assertEqual(self.note.text, note.text)
+        self.assertEqual(self.note.slug, note.slug)
+        self.assertEqual(self.note.author, note.author)
+
+    def test_authorized_user_cant_see_notes_from_other_people(self):
+        self.assertNotIn(
+            self.note,
+            self.authenticated_client.get(
+                UrlsConstants.NOTES_LIST_URL
+            ).context['object_list']
+        )
 
     def test_pages_has_form(self):
-        for name, page_name, args in (
-            ('notes:add', 'добавления новой заметки', None),
-            ('notes:edit', 'редактирования заметки', (self.note.slug,))
+        for url in (
+            UrlsConstants.NOTE_ADD_URL,
+            UrlsConstants.NOTE_EDIT_URL
         ):
-            with self.subTest(name=name):
-                response = self.author_client.get(reverse(name, args=args))
-                self.assertIn(
-                    'form',
-                    response.context,
-                    (
-                        'Убедитесь, что объект form есть в словаре '
-                        f'контекста для страницы {page_name}.'
-                    )
-                )
+            with self.subTest(url=url):
                 self.assertIsInstance(
-                    response.context['form'],
-                    NoteForm,
-                    (
-                        'Убедитесь, что объект form относится к классу '
-                        'NoteForm.'
-                    )
+                    self.author_client.get(url).context.get('form'),
+                    NoteForm
                 )
