f19c05af9e10e39bc4f82d06334ac10494c8c94b
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2024-04-13 19:03:29 +0300

    Update test_routes.py in the notes app

--- ya_note/notes/tests/test_routes.py
+++ ya_note/notes/tests/test_routes.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 
-from .constants import ConstantsUrls
+from .constants import UrlsConstants
 from .set_up_test_data import TestData
 
 
@@ -12,64 +12,63 @@ class TestRoutes(TestData):
 
     def test_pages_availability(self):
         for url, client, status in (
-            (ConstantsUrls.HOMEPAGE_URL,
+            (UrlsConstants.HOMEPAGE_URL,
              self.client,
              HTTPStatus.OK),
-            (ConstantsUrls.LOGIN_URL,
+            (UrlsConstants.LOGIN_URL,
              self.client,
              HTTPStatus.OK),
-            (ConstantsUrls.LOGOUT_URL,
+            (UrlsConstants.LOGOUT_URL,
              self.client,
              HTTPStatus.OK),
-            (ConstantsUrls.SIGNUP_URL,
+            (UrlsConstants.SIGNUP_URL,
              self.client,
              HTTPStatus.OK),
-            (ConstantsUrls.NOTE_ADD_URL,
+            (UrlsConstants.NOTE_ADD_URL,
              self.authenticated_client,
              HTTPStatus.OK),
-            (ConstantsUrls.NOTES_LIST_URL,
+            (UrlsConstants.NOTES_LIST_URL,
              self.authenticated_client,
              HTTPStatus.OK),
-            (ConstantsUrls.SUCCESS_URL,
+            (UrlsConstants.SUCCESS_URL,
              self.authenticated_client,
              HTTPStatus.OK),
-            (ConstantsUrls.NOTE_DETAIL_URL,
+            (UrlsConstants.NOTE_DETAIL_URL,
              self.author_client,
              HTTPStatus.OK),
-            (ConstantsUrls.NOTE_EDIT_URL,
+            (UrlsConstants.NOTE_EDIT_URL,
              self.author_client,
              HTTPStatus.OK),
-            (ConstantsUrls.NOTE_DELETE_URL,
+            (UrlsConstants.NOTE_DELETE_URL,
              self.author_client,
              HTTPStatus.OK),
-            (ConstantsUrls.NOTE_DETAIL_URL,
+            (UrlsConstants.NOTE_DETAIL_URL,
              self.authenticated_client,
              HTTPStatus.NOT_FOUND),
-            (ConstantsUrls.NOTE_EDIT_URL,
+            (UrlsConstants.NOTE_EDIT_URL,
              self.authenticated_client,
              HTTPStatus.NOT_FOUND),
-            (ConstantsUrls.NOTE_DELETE_URL,
+            (UrlsConstants.NOTE_DELETE_URL,
              self.authenticated_client,
              HTTPStatus.NOT_FOUND)
         ):
             with self.subTest(url=url, client=client, status=status):
-                response = client.get(url)
-                self.assertEqual(response.status_code, status)
+                self.assertEqual(client.get(url).status_code, status)
 
     def test_redirect_for_anonymous_user(self):
         for url, redirect in (
-            (ConstantsUrls.NOTE_ADD_URL,
-             ConstantsUrls.REDIRECT_TO_NOTE_ADD_URL),
-            (ConstantsUrls.NOTES_LIST_URL,
-             ConstantsUrls.REDIRECT_TO_NOTES_LIST_URL),
-            (ConstantsUrls.SUCCESS_URL,
-             ConstantsUrls.REDIRECT_TO_SUCCESS_URL),
-            (ConstantsUrls.NOTE_DETAIL_URL,
-             ConstantsUrls.REDIRECT_TO_NOTE_DETAIL_URL),
-            (ConstantsUrls.NOTE_EDIT_URL,
-             ConstantsUrls.REDIRECT_TO_NOTE_EDIT_URL),
-            (ConstantsUrls.NOTE_DELETE_URL,
-             ConstantsUrls.REDIRECT_TO_NOTE_DELETE_URL)
+            (UrlsConstants.NOTE_ADD_URL,
+             UrlsConstants.REDIRECT_TO_NOTE_ADD_URL),
+            (UrlsConstants.NOTES_LIST_URL,
+             UrlsConstants.REDIRECT_TO_NOTES_LIST_URL),
+            (UrlsConstants.SUCCESS_URL,
+             UrlsConstants.REDIRECT_TO_SUCCESS_URL),
+            (UrlsConstants.NOTE_DETAIL_URL,
+             UrlsConstants.REDIRECT_TO_NOTE_DETAIL_URL),
+            (UrlsConstants.NOTE_EDIT_URL,
+             UrlsConstants.REDIRECT_TO_NOTE_EDIT_URL),
+            (UrlsConstants.NOTE_DELETE_URL,
+             UrlsConstants.REDIRECT_TO_NOTE_DELETE_URL)
         ):
             with self.subTest(url=url, redirect=redirect):
                 self.assertRedirects(self.client.get(url), redirect)
