bbdf3ff40e6cb041ba8a3fe2725b39a76fd5c3c5
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2024-04-15 19:19:23 +0300

    Update conftest.py in the news app

--- ya_news/news/pytest_tests/conftest.py
+++ ya_news/news/pytest_tests/conftest.py
@@ -1,12 +1,11 @@
 from datetime import datetime, timedelta
-import pytest
 
 from django.test.client import Client
+import pytest
 from django.urls import reverse
 from django.conf import settings
 from django.utils import timezone
 
-from news.forms import BAD_WORDS
 from news.models import Comment, News
 
 
@@ -78,16 +77,6 @@ def a_lot_of_comments(news, author):
 
 
 @pytest.fixture
-def news_id_for_args(news):
-    return (news.id,)
-
-
-@pytest.fixture
-def comment_id_for_args(comment):
-    return (comment.id,)
-
-
-@pytest.fixture
 def login_url():
     return reverse('users:login')
 
@@ -103,39 +92,35 @@ def signup_url():
 
 
 @pytest.fixture
-def home_url():
+def homepage_url():
     return reverse('news:home')
 
 
 @pytest.fixture
-def detail_url(news_id_for_args):
-    return reverse('news:detail', args=news_id_for_args)
+def news_detail_url(news):
+    return reverse('news:detail', args=(news.id,))
 
 
 @pytest.fixture
-def edit_url(comment_id_for_args):
-    return reverse('news:edit', args=comment_id_for_args)
+def comment_edit_url(comment):
+    return reverse('news:edit', args=(comment.id,))
 
 
 @pytest.fixture
-def delete_url(comment_id_for_args):
-    return reverse('news:delete', args=comment_id_for_args)
+def comment_delete_url(comment):
+    return reverse('news:delete', args=(comment.id,))
 
 
 @pytest.fixture
-def url_to_comments(detail_url):
-    return detail_url + '#comments'
+def redirect_to_comments_url(news_detail_url):
+    return news_detail_url + '#comments'
 
 
 @pytest.fixture
-def form_data():
-    return {
-        'text': 'Новый текст комментария'
-    }
+def redirect_from_login_to_comment_edit_url(login_url, comment_edit_url):
+    return f'{login_url}?next={comment_edit_url}'
 
 
 @pytest.fixture
-def bad_words_data():
-    return {
-        'text': f'Тест, {BAD_WORDS[0]}, больше текста'
-    }
+def redirect_from_login_to_comment_delete_url(login_url, comment_delete_url):
+    return f'{login_url}?next={comment_delete_url}'
