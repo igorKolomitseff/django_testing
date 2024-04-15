508b0f45975d82f94a8f9932e751e9a365205caf
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2024-04-15 19:21:23 +0300

    Refactor test_routes.py in the news app

--- ya_news/news/pytest_tests/test_routes.py
+++ ya_news/news/pytest_tests/test_routes.py
@@ -1,76 +1,55 @@
 from http import HTTPStatus
-import pytest
 
 from pytest_django.asserts import assertRedirects
+import pytest
 
 
-@pytest.mark.django_db
+HOMEPAGE_URL = pytest.lazy_fixture('homepage_url')
+LOGIN_URL = pytest.lazy_fixture('login_url')
+LOGOUT_URL = pytest.lazy_fixture('logout_url')
+SIGNUP_URL = pytest.lazy_fixture('signup_url')
+NEWS_DETAIL_URL = pytest.lazy_fixture('news_detail_url')
+COMMENT_EDIT_URL = pytest.lazy_fixture('comment_edit_url')
+COMMENT_DELETE_URL = pytest.lazy_fixture('comment_delete_url')
+
+ANONYMOUS_CLIENT = pytest.lazy_fixture('client')
+AUTHOR_CLIENT = pytest.lazy_fixture('author_client')
+READER_CLIENT = pytest.lazy_fixture('reader_client')
+
+REDIRECT_FROM_LOGIN_URL_TO_COMMENT_EDIT_URL = pytest.lazy_fixture(
+        'redirect_from_login_to_comment_edit_url'
+    )
+REDIRECT_FROM_LOGIN_URL_TO_COMMENT_DELETE_URL = pytest.lazy_fixture(
+        'redirect_from_login_to_comment_delete_url'
+    )
+
+pytestmark = pytest.mark.django_db
+
+
 @pytest.mark.parametrize(
-    'url, page_name',
+    'url, parametrized_client, expected_status',
     (
-        (pytest.lazy_fixture('home_url'),
-         'домашняя страница'),
-        (pytest.lazy_fixture('detail_url'),
-         'страница отдельной новости'),
-        (pytest.lazy_fixture('login_url'),
-         'страница входа в учётную запись'),
-        (pytest.lazy_fixture('logout_url'),
-         'страница выхода из учётной записи'),
-        (pytest.lazy_fixture('signup_url'),
-         'страница регистрации'),
+        (HOMEPAGE_URL, ANONYMOUS_CLIENT, HTTPStatus.OK),
+        (LOGIN_URL, ANONYMOUS_CLIENT, HTTPStatus.OK),
+        (LOGOUT_URL, ANONYMOUS_CLIENT, HTTPStatus.OK),
+        (SIGNUP_URL, ANONYMOUS_CLIENT, HTTPStatus.OK),
+        (NEWS_DETAIL_URL, ANONYMOUS_CLIENT, HTTPStatus.OK),
+        (COMMENT_EDIT_URL, AUTHOR_CLIENT, HTTPStatus.OK),
+        (COMMENT_DELETE_URL, AUTHOR_CLIENT, HTTPStatus.OK),
+        (COMMENT_EDIT_URL, READER_CLIENT, HTTPStatus.NOT_FOUND),
+        (COMMENT_DELETE_URL, READER_CLIENT, HTTPStatus.NOT_FOUND)
     )
 )
-def test_pages_availability_for_anonymous_user(client, url, page_name):
-    response = client.get(url)
-    assert response.status_code == HTTPStatus.OK, (
-        f'Убедитесь, что {page_name} доступна анонимному пользователю.'
-    )
+def test_pages_availability(url, parametrized_client, expected_status):
+    assert parametrized_client.get(url).status_code == expected_status
 
 
 @pytest.mark.parametrize(
-    'parametrized_client, expected_status, user, status_name',
+    'url, expected_url',
     (
-        (pytest.lazy_fixture('author_client'), HTTPStatus.OK,
-         'автора комментария', 'доступна'),
-        (pytest.lazy_fixture('reader_client'), HTTPStatus.NOT_FOUND,
-         'стороннего аутентифицированного пользователя',
-         'для чужого комментария недоступна'),
-    ),
-)
-@pytest.mark.parametrize(
-    'url, page_name',
-    (
-        (pytest.lazy_fixture('edit_url'),
-         'страница редактирования комментария'),
-        (pytest.lazy_fixture('delete_url'),
-         'страница удаления комментария'),
-    ),
-)
-def test_availability_for_comment_edit_and_delete(
-    parametrized_client, url, expected_status, user, status_name, page_name
-):
-    response = parametrized_client.get(url)
-    assert response.status_code == expected_status, (
-        f'Убедитесь, что {page_name} {status_name} для {user}.'
-    )
-
-
-@pytest.mark.django_db
-@pytest.mark.parametrize(
-    'url, page_name',
-    (
-        (pytest.lazy_fixture('edit_url'),
-         'страницу редактирования комментария'),
-        (pytest.lazy_fixture('delete_url'),
-         'страницу удаления комментария'),
+        (COMMENT_EDIT_URL, REDIRECT_FROM_LOGIN_URL_TO_COMMENT_EDIT_URL),
+        (COMMENT_DELETE_URL, REDIRECT_FROM_LOGIN_URL_TO_COMMENT_DELETE_URL)
     )
 )
-def test_redirect_for_anonymous_client(client, login_url, url, page_name):
-    assertRedirects(
-        client.get(url),
-        f'{login_url}?next={url}',
-        msg_prefix=(
-            f'Убедитесь, что при попытке перейти на {page_name} '
-            f'анонимный пользователь перенаправляется на страницу авторизации.'
-        )
-    )
+def test_redirect_for_anonymous_client(client, url, expected_url):
+    assertRedirects(client.get(url), expected_url)
